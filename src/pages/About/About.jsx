c1df95c168a22b2185d0e2e1904d232d91dcd5b0
Pawan70056 <ssmp70056@gmail.com>
2025-02-28 20:33:37 +0545

    New about file

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import U_construction from '../../component/U_construction'
+
+export default function About() {
+  return (
+    <div className='mt-10'>
+      <U_construction/>
+    </div>
+  )
+}
