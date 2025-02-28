f681458c5bd51ba81a0c461398b947fa8a1b6f3e
Pawan70056 <ssmp70056@gmail.com>
2025-02-28 20:36:38 +0545

    New Showroom file

---
+++ src/pages/Showroom/Showroom.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import U_construction from '../../component/U_construction'
+
+export default function Showroom() {
+  return (
+    <div className='mt-10'>
+    <U_construction/>
+  </div>
+  )
+}
